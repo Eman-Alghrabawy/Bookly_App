42c56ccb48b7547c526d590aae7a0486f5a25248
Eman-Alghrabawy <emangh2099@gmail.com>
2023-09-19 12:44:00 +0300

    refactor book details view

---
+++ lib/features/home/views/widgets/books_details_section.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_booky_1/core/utils/styles.dart';
+import 'package:flutter_booky_1/features/home/views/widgets/book_rating.dart';
+import 'package:flutter_booky_1/features/home/views/widgets/books_action.dart';
+import 'package:flutter_booky_1/features/home/views/widgets/custom_book_item.dart';
+
+class BookDetailsSection extends StatelessWidget {
+  const BookDetailsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: width * 0.2),
+          child: const CustomBookImage(),
+        ),
+        const SizedBox(
+          height: 43,
+        ),
+        Text(
+          'Little Women',
+          style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(
+          height: 6,
+        ),
+        Opacity(
+          opacity: 0.7,
+          child: Text(
+            "Louisa May Alcott's",
+            style: Styles.textStyle18.copyWith(
+                fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
+          ),
+        ),
+        const SizedBox(
+          height: 18,
+        ),
+        const BookRating(
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+        const SizedBox(
+          height: 37,
+        ),
+        const BooksAction(),
+      ],
+    );
+  }
+}
